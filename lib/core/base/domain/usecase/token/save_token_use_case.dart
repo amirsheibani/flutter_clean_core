48848c280f11264fadad71e075d02c8ca87fe947
amirsheibani <a_sheibani@keywa.ca>
2023-06-14 16:40:53 +0330

    set color pallet on themeData

--- lib/core/base/domain/usecase/token/save_token_use_case.dart
+++ lib/core/base/domain/usecase/token/save_token_use_case.dart
@@ -1,6 +1,3 @@
-import 'dart:js' as js;
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter_clean_core/core/base/domain/entities/token_entity.dart';
 import 'package:flutter_clean_core/core/base/domain/repositories/token/token_repository.dart';
 import 'package:flutter_clean_core/core/base/domain/usecase/base_usecase.dart';
@@ -16,12 +13,12 @@ class SaveTokenUseCase extends BaseUseCase<Result<bool>, TokenEntity?> {
   @override
   Future<Result<bool>> call(TokenEntity? arg) {
     TokenEntity? tokenEntity = arg;
-    if (kIsWeb) {
-      var jsContext = js.context;
-      var kcToken = jsContext["kc"]["token"];
-      var kcTokenRefresh = jsContext["kc"]["refreshToken"];
-      tokenEntity = TokenEntity(refreshToken: kcTokenRefresh, accessToken: kcToken);
-    }
+    // if (kIsWeb) {
+    //   var jsContext = js.context;
+    //   var kcToken = jsContext["kc"]["token"];
+    //   var kcTokenRefresh = jsContext["kc"]["refreshToken"];
+    //   tokenEntity = TokenEntity(refreshToken: kcTokenRefresh, accessToken: kcToken);
+    // }
     return _repository.saveToken(tokenEntity!);
   }
 }
