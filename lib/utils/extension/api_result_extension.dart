ae2e2194bacf656d033a52fcb8104404323e9212
Amir Sheibani <a_sheibani@keywa.ca>
2023-06-19 16:08:20 +0330

    fix SocketException on ErrorApiResultExtension

--- lib/utils/extension/api_result_extension.dart
+++ lib/utils/extension/api_result_extension.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:flutter_clean_core/core/base/data/remote/models/base_response.dart';
 import 'package:flutter_clean_core/core/service/network/network_state/api_result.dart';
 
@@ -5,9 +8,14 @@ import 'network_exceptions.dart';
 
 extension ErrorApiResultExtension on Object {
   Result<T> toResult<T>({StackTrace? stackTrace}) {
-    final error = NetworkExceptions.getDioException(this, stackTrace);
-    final str = NetworkExceptions.getErrorMessage(error ?? const NetworkExceptions.unexpectedError());
-    return Result.failure(message: str);
+    String message = '';
+    if (this is DioException) {
+      final error = NetworkExceptions.getDioException(this, stackTrace);
+      message = NetworkExceptions.getErrorMessage(error ?? const NetworkExceptions.unexpectedError());
+    } else if (this is SocketException) {
+      message = stackTrace.toString();
+    }
+    return Result.failure(message: message);
   }
 }
 
